a98f81d01c8031067c9322b637b5de147e3d175c
Rohit Ramu <rohitramu@gmail.com>
2017-07-24 15:48:27 -0700

    Simplify button action code

--- src/popup/menu.js
+++ src/popup/menu.js
@@ -1,20 +1,15 @@
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("action")) {
-
-    // browser.tabs.executeScript(null, {
-    //   code:"document.body.style.border = '5px solid red'"
-    // });
-    chrome.tabs.executeScript(null,
-      { code:"document.body.style.border = '5px solid red'" }
-    );
+saveLocalStorage = function() {
+  
   window.close();
-  }
-});
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
-});
+loadLocalStorage = function() {
+  
+  window.close();
+};
+
+clearLocalStorage = function() {
+  
+  window.close();
+};
 
