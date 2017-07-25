884f6b79e41d957184cf1ff53d9805569ee05567
Daniel Jost <dajost@microsoft.com>
2017-07-25 12:50:49 -0700

    More environment code.

--- src/popup/environments/menu-environments.js
+++ src/popup/environments/menu-environments.js
@@ -1,29 +1,61 @@
-// Change active env on click
+/// Startup code
+var activeEnv = "";
 
-// Save from localStorage
+$('form').on('submit', function(event) {
+  newEnvironment($('#newEnvironment').val());
+  $('#newEnvironment').val('');
+  event.preventDefault();
+});
+
+newEnvironment("Test Environment");
+newEnvironment("Test 2");
+
+/// Functions
+// Click handler
+var clickHandler = function(click) {
+  var target = click.target;
+  if (target.className === "pure-menu-link") {
+    changeActiveEnvironment(target);
+  }
+};
+document.addEventListener("click", clickHandler);
+
+// Change active environment
+function changeActiveEnvironment(target) {
+  // If target is current, take no action
+  if (target === activeEnv) {
+    return;
+  }
+  // disable last active
+  else if (activeEnv) {
+    $(activeEnv.nextSibling).removeClass('active');
+  }
+
+  // set new active
+  activeEnv = target;
+  $(target.nextSibling).addClass('active');
+}
+
+// Save from localStorage chrome.storage.sync
 
 // Load from localStorage
 
 // Add new env to localStorage and menu
 function newEnvironment(name) {
   // build the section header
-  var listItem = $('<li />');
-  listItem.addClass('pure-menu-item');
-  $('#env-list').append(listItem);
-  var link = $('<a>' + name + '</a>');
-  link.addClass('pure-menu-link');
+  var listItem = $('<li />').addClass('pure-menu-item');
+  var link = $('<a>' + name + '</a>').addClass('pure-menu-link');
   listItem.append(link);
 
   // build the table
-  var table = $('<table></table>').addClass('pure-table pure-table-horizontal active');
+  var table = $('<table></table>').addClass('pure-table pure-table-horizontal');
   var tHead = $('<thead><tr><th>From</th><th>To</th></tr></thead>');
   table.append(tHead);
   var tBody = $('<tbody><tr><td>test</td><td>test2</td></tr></tbody>');
   table.append(tBody);
-  $('#env-list').append(table);
+  listItem.append(table);
+  $('#envList').append(listItem);
 }
-
-newEnvironment("Test Environment");
 
 // Add new item to localStorage and menu
 
