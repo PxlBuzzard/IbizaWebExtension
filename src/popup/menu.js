396f256b40aeee6a4b568b3153237cc16c62948e
Rohit Ramu <rohitramu@gmail.com>
2017-07-24 18:32:56 -0700

    Convert buttons to use event handlers

--- src/popup/menu.js
+++ src/popup/menu.js
@@ -1,8 +1,10 @@
 saveLocalStorage = function() {
   a = new User();
-  utils.getBrowserContext(function(t, w) {
-    console.log(t);
-    console.log(w);
+  Utils.getBrowserContext(function(currentTab, currentWindow) {
+    console.log(currentTab);
+    console.log(currentWindow);
+
+    // TODO: do stuff with tab and window
   });
 
   sessionStorage.set()
@@ -10,12 +12,34 @@ saveLocalStorage = function() {
 };
 
 loadLocalStorage = function() {
-  
+  Utils.getBrowserContext(function(currentTab, currentWindow) {
+    console.log(currentTab);
+    console.log(currentWindow);
+
+    // TODO: do stuff with tab and window
+  });
   window.close();
 };
 
 clearLocalStorage = function() {
-  
+  Utils.getBrowserContext(function(currentTab, currentWindow) {
+    console.log(currentTab);
+    console.log(currentWindow);
+
+    // TODO: do stuff with tab and window
+  });
   window.close();
 };
 
+clickHandler = function(click) {
+  var button = click.target;
+  if (button.id === "saveLocalStorageButton") {
+    saveLocalStorage();
+  } else if (button.id === "loadLocalStorageButton") {
+    loadLocalStorage();
+  } else if (button.id === "clearLocalStorageButton") {
+    clearLocalStorage();
+  }
+};
+
+document.addEventListener('click', clickHandler);
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-getBrowserContext = function(callback) {
-  chrome.tabs.getSelected(function(selectedTab) {
-    chrome.windows.get(selectedTab.windowId, function(selectedWindow) {
-      callback(selectedTab, selectedWindow);
+const Utils = {
+  getBrowserContext: function(callback) {
+    chrome.tabs.getSelected(function(selectedTab) {
+      chrome.windows.get(selectedTab.windowId, function(selectedWindow) {
+        callback(selectedTab, selectedWindow);
+      });
     });
-  });
-};
-
-export default utils;
+  }
+}
