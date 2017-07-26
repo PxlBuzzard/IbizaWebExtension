0111bdac464fd46cbc03699fd09ff30d3fbbff7c
Cecilia Liu <xiaoxliu@microsoft.com>
2017-07-25 17:32:29 -0700

    removed user

--- src/popup/user_sessions/user.js
+++
@@ -1,7 +0,0 @@
-class User {
-    constructor(localStorage, sessionStorage, cookieStorage) {
-        this.local = localStorage;
-        this.session = sessionStorage;
-        this.cookie = cookieStorage;
-    }
-}
