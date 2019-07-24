14861369855a28e1868f4a13fc16f529a1be5cb1
Daniel Jost <dajost@microsoft.com>
2019-07-23 18:02:44 -0700

    Added Apply button (with no action yet).

---
+++ src/popup/components/Apply.vue
@@ -0,0 +1,20 @@
+<template>
+<section>
+    <button class="button is-success" @click="apply">Apply</button>
+</section>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Apply",
+  methods: {
+    apply() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
