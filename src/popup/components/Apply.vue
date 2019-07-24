6b9bb622fd97410ecd0b8b7f6e9337718b5308bb
Daniel Jost <dajost@microsoft.com>
2019-07-24 14:37:59 -0700

    All FeatureGroups can be applied.

--- src/popup/components/Apply.vue
+++ src/popup/components/Apply.vue
@@ -7,7 +7,7 @@
 <script lang="ts">
 import UrlParser from "../url/UrlParser";
 import Vue from "vue";
-import { IEnvironment, IFeature } from "../config/Schema";
+import { IEnvironment, IFeature, IFeatureGroup } from "../config/Schema";
 
 export default Vue.extend({
   name: "Apply",
@@ -18,13 +18,15 @@ export default Vue.extend({
         let env = this.config.environments.filter((e: IEnvironment) => e.label === this.currentEnv)[0];
 
         let finalQueries: StringMap<string> = {};
-        if (this.featureGroups[0].features != undefined) {
-            this.featureGroups[0].features.forEach((query: IFeature) => {
-                if (query.selected != undefined) {
-                    finalQueries[query.name] = query.selected;
-                }
-            });
-        }
+        this.featureGroups.forEach((group: IFeatureGroup) => {
+            if (group.features != undefined) {
+                group.features.forEach((query: IFeature) => {
+                    if (query.selected != undefined) {
+                        finalQueries[query.name] = query.selected;
+                    }
+                });
+            }
+        });
 
         urlParser.setUrl({
             origin: `https://${env.host}`,
