38e2a8a2d8eb3a414b3ad1df5601b45dda613e7d
Daniel Jost <dajost@microsoft.com>
2019-07-23 19:11:24 -0700

    Removed more old files.

--- src/popup/utils.js
+++
@@ -1,9 +0,0 @@
-const Utils = {
-  getBrowserContext: function(callback) {
-    chrome.tabs.getSelected(function(selectedTab) {
-      chrome.windows.get(selectedTab.windowId, function(selectedWindow) {
-        callback(selectedTab, selectedWindow);
-      });
-    });
-  }
-}
